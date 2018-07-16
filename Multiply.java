5077fab247e66a254c72bf1ef47fa02f3141d470
JayaPradha003 <41284767+JayaPradha003@users.noreply.github.com>
2018-07-16 18:10:06 +0530

    Create Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			int a=n*i;
+				System.out.println(a);
+		}
+		
+
+	}
+
+}

ea0d2ab268f3384e8e0a7e2e98f45d4c882cc7dc
JayaPradha003 <41284767+JayaPradha003@users.noreply.github.com>
2018-07-16 15:41:53 +0530

    Create Hell0N.java

---
+++ Hell0N.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Hell0N {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner (System.in);
+		int n=sc.nextInt();
+		String str="Hello";
+		for(int i=0;i<n;i++) {
+			System.out.println(str);
+		}
+		
+
+	}
+
+}

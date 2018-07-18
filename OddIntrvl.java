123d9b830afdaa1d8ec1803162619ef8615496d7
JayaPradha003 <41284767+JayaPradha003@users.noreply.github.com>
2018-07-18 14:01:18 +0530

    Create OddIntrvl.java

---
+++ OddIntrvl.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class OddIntrvl {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		int a=n+1;
+		for(int i=a;i<m;i++) {
+			if(i%2!=0) {
+				System.out.println(i);
+			}
+		}
+		
+
+	}
+
+}

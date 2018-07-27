db6f0470b5be01a31daf1740ab6f9fe2592950a7
JayaPradha003 <41284767+JayaPradha003@users.noreply.github.com>
2018-07-27 10:06:34 +0530

    Create Median.java

---
+++ Median.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Median {
+
+	public static void main(String[] args) {
+		int a[]= {2,3,1,45,5};
+		Arrays.sort(a);
+		int med=a[a.length/2];
+		System.out.println(med);
+		
+	}
+		
+
+	}
+	
+
+

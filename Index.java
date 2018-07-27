41dbbd8de8b79114ad3ed946cea2f858107f87fe
JayaPradha003 <41284767+JayaPradha003@users.noreply.github.com>
2018-07-27 14:21:17 +0530

    Create Index.java

---
+++ Index.java
@@ -0,0 +1,12 @@
+public class Index {
+
+	public static void main(String[] args) {
+	int a[]= {2,1,3};
+	for(int i=0;i<a.length;i++) {
+		System.out.print(a[i]);
+		System.out.println(" "+i);
+	}
+	}
+}
+
+

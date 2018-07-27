0b1ac35d102524be4f1ad60dfbf98a690d244f44
JayaPradha003 <41284767+JayaPradha003@users.noreply.github.com>
2018-07-27 09:47:18 +0530

    Create Max.java

---
+++ Max.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Max {
+
+	public static void main(String[] args) {
+		int a[]= {-2, 3, 1, -45, 5};
+		Arrays.sort(a);
+		int max=a[a.length-1];
+		System.out.println(max);
+		
+	}
+		
+
+	}

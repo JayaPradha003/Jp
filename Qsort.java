c685da956589aa8e8eafee34b7d3901134f69697
JayaPradha003 <41284767+JayaPradha003@users.noreply.github.com>
2018-07-27 10:09:45 +0530

    Create Qsort.java

---
+++ Qsort.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Qsort {
+
+	public static void main(String[] args) {
+		int a[]= {2,3,1,45,5};
+		Arrays.sort(a);
+		
+		System.out.println(Arrays.toString(a));
+		
+	}
+		
+
+	}
+	
+
+

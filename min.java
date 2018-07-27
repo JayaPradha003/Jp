13c805ef68be8e842edf359d11a5103a45a6eb76
JayaPradha003 <41284767+JayaPradha003@users.noreply.github.com>
2018-07-27 09:48:44 +0530

    Create min.java

---
+++ min.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Min{
+
+	public static void main(String[] args) {
+		int a[]= {-2, 3, 1, -45, 5};
+		Arrays.sort(a);
+		int min=a[0];
+		System.out.println(min);
+		
+	}
+		
+
+	}
+	

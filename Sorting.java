f55edd1302de3b67817d917e3775d4c43afa4b22
JayaPradha003 <41284767+JayaPradha003@users.noreply.github.com>
2018-07-27 09:56:47 +0530

    Create Sorting.java

---
+++ Sorting.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Sorting {
+
+	public static void main(String[] args) {
+		int a[]= {4,5,2,7,1,9};
+		Arrays.sort(a);
+		System.out.println(Arrays.toString(a));
+		
+	}
+		
+
+	}
+	
+
+

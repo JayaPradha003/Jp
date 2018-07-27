45fc7ed850c877548ac3b91c9c40bc4560ffe459
JayaPradha003 <41284767+JayaPradha003@users.noreply.github.com>
2018-07-27 14:44:23 +0530

    Create SubTm.java

---
+++ SubTm.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SubTm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int d=sc.nextInt();
+		System.out.print(Math.abs(a-b));
+		System.out.println(" "+Math.abs(c-d));
+		}
+	
+
+	}
+
